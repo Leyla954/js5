8866485472f06718af4bb7924f0d7f440e05ddfd
Leyla Muradzade <lvmuradzade55@gmail.com>
2024-11-06 17:56:31 +0400

    Add files via upload

---
+++ js5/main.js
@@ -0,0 +1,100 @@
+
+
+// 1)
+// Bir array yaradin adlardan ibaret olsun hemin arraya her hansisa bir adi
+// arrayin metodlarindan isdifade etmekle daxil edin daha sonra silin
+
+
+// let names = ["Nail", "Araz", "Sema"]
+// names.push("Anar")
+// console.log(names);
+// names.pop();
+//  console.log(names);
+
+
+
+// 2)
+// let obj = [
+//     {
+//         name: "Ali",
+//         surname: "Aliyev",
+//         age: 10,
+//         driving: true
+//     }, {
+//         name: "Veli",
+//         surname: "Aliyev",
+//         age: 17,
+//         driving: false,
+//     }, {
+//         name: "Mehman",
+//         surname: "Aliyev",
+//         age: 25,
+//         driving: true,
+//     },{
+//         name: "Nicat",
+//         surname: "Aliyev",
+//         age:26,
+//         driving: false
+//     },{
+//         name: "Ayxan",
+//         surname: "Aliyev",
+//         age:15,
+//         driving: true
+//     },{
+//         name: "Nihad",
+//         surname: "Aliyev",
+//         age:19,
+//         driving: true
+//     }
+// ]
+
+
+// 3)
+// bu arrayde adi n ile baslayanlari loga cixaracaq funksiya qurun
+// yasi 20 den boyuk ve 26 den kicik olan objectleri console.loga cixaran funksiya yazin
+// yasi cut olan ve suruculuk vesieqesi olanlari loga cixaran funksiya yazin
+
+
+
+// function userName (){
+//     for (let i=0; i< 6; i++){
+//         if(obj[i].name == N){
+//             console.log(obj[i]);
+
+//         }
+//     }
+// }
+// userName ()
+
+
+// function age ( ){
+//     for (let i=0; i< 6; i++){
+//         if(obj[i].age > 20 && obj[i].age < 26){
+//             console.log(obj[i]);
+
+//         }
+//     }
+// }
+// age()
+
+
+// function age(){
+//         for (let i=0; i< 6; i++){
+//             if(obj[i].age % 2 == 0 && obj[i].driving == true){
+//                 console.log(obj[i]);
+
+//             }
+//         }
+//     }
+//     age()
+
+
+// 4)
+// obj arrayini tersine yazdirin yeni baslangic deyer Ali deyil Nihad olsun
+
+// obj.reverse()
+//  console.log(obj);
+
+
+
+
